00ee1c8a454d0c0617239ad816afc072dbf13c49
Erick_B <Erick_B@LAB3-18>
2023-03-30 08:21:52 -0600

    Agregar archivos de proyecto.

---
+++ PartidosPoliticos1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExamenParcial
+{
+    class PartidosPoliticos1
+    {
+        String nombrePartido;
+        String dpi;
+        DateTime fecha;
+
+        public string NombrePartido { get => nombrePartido; set => nombrePartido = value; }
+        public string Dpi { get => dpi; set => dpi = value; }
+        public DateTime Fecha { get => fecha; set => fecha = value; }
+    }
+}
